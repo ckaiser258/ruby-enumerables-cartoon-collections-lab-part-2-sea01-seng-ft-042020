6667c3d07729c024355996bbbbc0973b7cd49e1c
Colton Kaiser <kaiserc2@seattleu.edu>
2020-04-01 22:45:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |call|
-    valid_calls.include? == call
+    valid_calls.include? call
   end
 end
